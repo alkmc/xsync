d1308626afc168da77248f04bed329858bc36e40
Aleksander Kmiecik <32b3@proton.me>
2026-09-11 23:20:27 +0200

    modernize error handling

--- singleflight/singleflight.go
+++ singleflight/singleflight.go
@@ -46,8 +46,8 @@ func newPanicError(v any) error {
 	// The first line of the stack trace is of the form "goroutine N [status]:"
 	// but by the time the panic reaches Do the goroutine may no longer exist
 	// and its status will have changed. Trim out the misleading line.
-	if line := bytes.IndexByte(stack[:], '\n'); line >= 0 {
-		stack = stack[line+1:]
+	if _, rest, ok := bytes.Cut(stack, []byte("\n")); ok {
+		stack = rest
 	}
 	return &panicError{value: v, stack: stack}
 }
@@ -100,7 +100,7 @@ func (g *Group) Do(key string, fn func() (any, error)) (v any, err error, shared
 
 		if e, ok := c.err.(*panicError); ok {
 			panic(e)
-		} else if c.err == errGoexit {
+		} else if errors.Is(c.err, errGoexit) {
 			runtime.Goexit()
 		}
 		return c.val, c.err, true
@@ -169,7 +169,7 @@ func (g *Group) doCall(c *call, key string, fn func() (any, error)) {
 			} else {
 				panic(e)
 			}
-		} else if c.err == errGoexit {
+		} else if errors.Is(c.err, errGoexit) {
 			// Already in the process of goexit, no need to call again
 		} else {
 			// Normal return
