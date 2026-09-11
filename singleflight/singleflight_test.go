93e4e2eb5d82c8fa0e087c3b48c0e484a35b9442
Aleksander Kmiecik <32b3@proton.me>
2026-09-11 23:40:55 +0200

    move ExampleGroup to its own file

--- singleflight/singleflight_test.go
+++ singleflight/singleflight_test.go
@@ -387,33 +387,3 @@ func TestPanicDoSharedByDoChan(t *testing.T) {
 		t.Errorf("Test subprocess failed, but the crash isn't caused by panicking in Do")
 	}
 }
-
-func ExampleGroup() {
-	g := new(Group[string, string])
-
-	block := make(chan struct{})
-	res1c := g.DoChan("key", func() (string, error) {
-		<-block
-		return "func 1", nil
-	})
-	res2c := g.DoChan("key", func() (string, error) {
-		<-block
-		return "func 2", nil
-	})
-	close(block)
-
-	res1 := <-res1c
-	res2 := <-res2c
-
-	// Results are shared by functions executed with duplicate keys.
-	fmt.Println("Shared:", res2.Shared)
-	// Only the first function is executed: it is registered and started with "key",
-	// and doesn't complete before the second function is registered with a duplicate key.
-	fmt.Println("Equal results:", res1.Val == res2.Val)
-	fmt.Println("Result:", res1.Val)
-
-	// Output:
-	// Shared: true
-	// Equal results: true
-	// Result: func 1
-}
